8b1e21fb0ed853c10f42f2590356bf68e3982e6f
tlh <tlh@ust.hk>
2022-10-04 00:55:57 +0800

    package

--- R/adms_path.R
+++ R/adms_path.R
@@ -15,14 +15,14 @@
 # ' plot(circular_buffer)
 # ' @export
 
-adms_path <- function(datetime, version){
+adms_path <- function(datetime, version, basedir = "/home/dataop/data/nmodel/adms"){
 
     yyyy = lubridate::year(datetime)
     yyyymm = paste0(yyyy, stringr::str_pad(lubridate::month(datetime), 2, pad = "0"))
     yyyymmdd = paste0(yyyymm, stringr::str_pad(lubridate::day(datetime), 2, pad = "0"))
     yyyymmddhh = paste0(yyyymmdd, stringr::str_pad(lubridate::hour(datetime), 2, pad = "0"))
 
-    path = paste0("/home/dataop/data/nmodel/adms/", version, "/fcstout.archive/",
+    path = paste0(basedir, '/', version, "/fcstout.archive/",
         yyyy, "/", yyyymm, "/", yyyymmdd, "/", yyyymmddhh, ".nc")
 
     if(!file.exists(path)) stop("ADMS file does not exist: {path}" |> glue::glue())
